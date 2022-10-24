9781086b678b48c1ca3b57a0c3b7b8131a000278
Eoin Costello <19255551@studentmail.ul.ie>
2022-10-24 18:27:23 +0100

    Changed setup steps, added tickets value to movies.csv, added ticket class

--- Movie.py
+++ Movie.py
@@ -1,9 +1,25 @@
+from Ticket import Ticket
+
 class Movie:
-    def __init__(self, title, length, t):
+    def __init__(self, title, length, t, tickets):
         self.title = title
         self.length = length
         self.t = t
+        self.tickets = tickets
+        self.screenNum = 0
 
+    ##Get Number of Tickets Left
+    def getTicketsLeft(self):
+        return self.tickets
+
+    ##Update Number of Tickets for Movie
+    def setTicketsLeft(self, tickets):
+        self.tickets = tickets
+
+    ##Set the Screen Movie is being Played in
+    def setScreenNum(self, screenNum):
+        self.screenNum = screenNum
+ 
     ##Return Movie Title & Length
     def printMovie(self) -> str:
         return "{} ({})".format(self.title, self.getMovieLength())
