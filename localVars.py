c7a1d5d544e6d2684c444a246d49975b09c50bc6
Eoin Costello <19255551@studentmail.ul.ie>
2022-11-03 16:08:04 +0000

    delete unused classes

--- localVars.py
+++
@@ -1,2 +0,0 @@
-# this file is going to be for any local environmental files you will need an exmaple could be
-image1path = "/Users/seanryan/Documents/cs4125-Project/Images/Image1"
--- routes/Admin.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, redirect, request
-import pandas
-
-app = Flask(__name__)
-
-
-@app.route('/admin', methods=['GET'])
-def adminPage():
-    return 'ho'
