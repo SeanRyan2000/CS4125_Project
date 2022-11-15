9f7a07d8003211a9b1a13092fa9de7f4a9499287
Eoin Costello <19255551@studentmail.ul.ie>
2022-11-15 15:28:18 +0000

    Deleted redundant classes

--- Cinema.py
+++
@@ -1,9 +0,0 @@
-from ScreenList import ScreenList
- ##SETUP CINEMA MODEL##
-class Cinema:
-    SCREEN_LIST = ScreenList.setUp()
-
-    ##Print all Screenings
-    for s in SCREEN_LIST:
-        print("Screen " + str(s.screenNum))
-        print(s.printMovies(), "\n")
