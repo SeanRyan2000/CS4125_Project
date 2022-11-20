04f0c7c9f65040df859e3388fdfb175a65614748
fiston kulimushi <fkulimushi5@gmail.com>
2022-11-20 16:36:00 +0000

    added the ticket price  movie as grid

--- routes/Movie.py
+++ routes/Movie.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from model.Admin.AddMovie import addMovieToCSV
+from routes.app import movie
 
+import os
+MOVIE_CSV_PATH_STRING = str(os.path.abspath('..')) + '/csv_files/movies.csv'
 
 class Movie:
     def __init__(self, title, length, t, tickets):
@@ -8,6 +12,23 @@ class Movie:
         self.type = ""
         self.tickets = tickets
         self.screenNum = 0
+        
+        email = pd.read_csv(MOVIE_CSV_PATH_STRING)
+  
+        self.userEmail = email['EMAIL'].tolist()
+ 
+    def register(self, user):
+        self.userEmail.append(user.getUserEmail())  
+        
+    def unregister(self, user):
+        self.userEmail.remove(user.getUserEmail())
+     
+    def notify_user(self, message):
+        
+        if(addMovieToCSV(movie)):
+            
+            for user in self.userEmail:
+                user.update(self, message)
 
     def getTicketsLeft(self):
         return self.tickets
